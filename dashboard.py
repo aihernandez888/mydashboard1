39ff1b3752fe4384fc378349de0b82ce8f686f02
aihernandez888 <ah22planning@gmail.com>
2025-06-01 02:17:55 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -127,7 +127,7 @@ if apod.get("media_type") == "image":
     nasa_img_html = f"""
     <div style="
         position: fixed;
-        top: 25px;
+        top: 50px;
         right: 20px;
         width: 250px;
         height: auto;
